77600aa262daf689840c3bd6b6040b657ac0be70
tomschweers <trschweers@gmail.com>
2018-08-21 16:17:50 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+## Setup
+
+```bash
+$ npm install --only=dev
+$ npm install
+```
+
+## Run development mode with hot reload
+
+```bash
+$ npm run dev
+```
+
+## Run build
+
+```bash
+$ npm run build
+```
+
+Compiled code will be in `./server/public/dist/` folder.
+
+## Deploy to Heroku
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+We set `NPM_CONFIG_PRODUCTION` to `false` to install `devDependencies`
+
+```bash
+$ heroku config:set NPM_CONFIG_PRODUCTION=false
+```
---
+++ client/src/App.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="app">
+    <img src="./assets/profile-pic.jpg" style="width:200px">
+    <h1></h1>
+    <h2>Social Links</h2>
+    <ul>
+      <li><a href="https://www.linkedin.com/in/tomschweers/" target="_blank">LinkedIn</a></li>
+      <li><a href="https://medium.com/@tomschweers" target="_blank">Medium</a></li>
+      <li><a href="https://stackoverflow.com/users/7891298/schws" target="_blank">Stack Overflow</a></li>
+      <li><a href="https://github.com/tomschweers" target="_blank">GitHub</a></li>
+    </ul>
+    <h2>Technologies</h2>
+    <ul>
+      <li><a href="https://nodejs.org/en/" target="_blank">Node.js</a></li>
+      <li><a href="https://expressjs.com/" target="_blank">Express.js</a></li>
+      <li><a href="https://vuejs.org/" target="_blank">Vue.js</a></li>
+      <li><a href="https://www.mongodb.com/" target="_blank">MongoDB</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
